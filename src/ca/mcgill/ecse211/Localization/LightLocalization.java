4f22fa5b298250fc4a54f6d673f4c8e9da213ce7
chaimaef <chaimaefahmi@outlook.com>
2018-11-03 13:02:17 -0400

    Fix Imports

--- src/ca/mcgill/ecse211/Localization/LightLocalization.java
+++ src/ca/mcgill/ecse211/Localization/LightLocalization.java
@@ -1,57 +1,39 @@
 package ca.mcgill.ecse211.Localization;
 
-import ca.mcgill.ecse211.odometer.Odometer;
 import ca.mcgill.ecse211.FinalProject.FinalProject;
 import ca.mcgill.ecse211.Navigation.Navigator;
-import ca.mcgill.ecse211.odometer.OdometerExceptions;
+import ca.mcgill.ecse211.odometer.Odometer;
 import lejos.hardware.Button;
-import lejos.hardware.Sound;
-import lejos.hardware.ev3.LocalEV3;
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
-import lejos.hardware.port.Port;
-import lejos.hardware.sensor.EV3ColorSensor;
+import lejos.hardware.sensor.EV3GyroSensor;
 import lejos.robotics.SampleProvider;
 
 public class LightLocalization {
 	private static double sensorOffset = -13.0;
-	static int counter = 1;
-	static float color;
-	static float lastColor;
-	static double temp;
-	static double yIntersectionplus;
-	static double yIntersectionminus;
-	static double xIntersectionplus;
-	static double xIntersectionminus;
-	static double deltatheta1;
-	static double deltatheta2;
-	static double xOrigin;
-	static double yOrigin;
-	static double y;
-	static double x;
-	static double tempDist;
+	private static int counter = 1;
+	private static float color;
+	private static float lastColor;
+	private static double temp;
+	private static double yIntersectionplus;
+	private static double yIntersectionminus;
+	private static double xIntersectionplus;
+	private static double xIntersectionminus;
+	private static double xOrigin;
+	private static double yOrigin;
 	private static double TRACK = FinalProject.getTrack();
 	private static double WHEEL_RAD = FinalProject.getWheelRad();
 	private static final EV3LargeRegulatedMotor leftMotor = FinalProject.leftMotor;
 	private static final EV3LargeRegulatedMotor rightMotor = FinalProject.rightMotor;
+	private static final Odometer odo = FinalProject.odo;
 	private static SampleProvider SColor = FinalProject.getColorBack();
-	static float[] data = FinalProject.getColorBufferBack();
-	public static Odometer odo;
+	private static float[] data = FinalProject.getColorBufferBack();
+	public static EV3GyroSensor gyro = FinalProject.gyro;
+
 	
 	public static void lightLocalize() {
 
-		try {
-			odo = Odometer.getOdometer(leftMotor, rightMotor, TRACK, WHEEL_RAD);
-		} catch (OdometerExceptions e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		
-		// FinalProject.odo.setTheta(0);
-		// Button.waitForAnyPress();
 		rightMotor.setSpeed(150);
 		leftMotor.setSpeed(150);
-
-		// SampleProvider SColor = sensor.getMode("Red");
 
 		SColor.fetchSample(data, 0);
 		color = data[0] * 1000;
@@ -60,14 +42,11 @@ public class LightLocalization {
 		leftMotor.rotate(-Navigator.convertAngle(WHEEL_RAD, TRACK, 450), true);
 
 		while (counter <= 4) {
-			// System.out.println(color);
 			lastColor = color;
 
 			SColor.fetchSample(data, 0);
 			color = data[0] * 1000;
-			// System.out.println(color);
 			if (color - lastColor > 5) {
-			//	Sound.beep();
 				temp = FinalProject.odo.getXYT()[2];
 				System.out.println(temp);
 				if (counter == 1) {
@@ -77,40 +56,34 @@ public class LightLocalization {
 					xIntersectionplus = temp;
 				}
 				if (counter == 3) {
-					yIntersectionplus = temp;	
+					yIntersectionplus = temp;
 				}
 				if (counter == 4) {
 					leftMotor.stop(true);
 					rightMotor.stop(false);
-					
+
 					xIntersectionminus = temp;
 				}
 				counter++;
 			}
 		}
-		
+
 		xOrigin = sensorOffset * Math.cos(Math.toRadians((yIntersectionminus - yIntersectionplus) / 2));
 		yOrigin = sensorOffset * Math.cos(Math.toRadians(xIntersectionplus - xIntersectionminus) / 2);
-		
-		FinalProject.odo.setX(xOrigin);
-		FinalProject.odo.setY(yOrigin);
-		
-		Navigator.travelTo(0, 0);	
-		
-		Navigator.turnTo(xIntersectionminus);
-		
-		FinalProject.gyro.reset();
-	
-		Navigator.turnTo((180+(xIntersectionplus-xIntersectionminus)/2)%360);
-		
-		FinalProject.gyro.reset();
-		
-//		System.out.println("x final:" + odo.getXYT()[0]);
-//		System.out.println("y final:" + odo.getXYT()[1]);
-//		System.out.println("t final:" + odo.getXYT()[2]);
 
-		
-		
+		odo.setX(xOrigin);
+		odo.setY(yOrigin);
+
+		Navigator.travelTo(0, 0);
+
+		Navigator.turnTo(xIntersectionminus);
+
+		gyro.reset();
+
+		Navigator.turnTo((180 + (xIntersectionplus - xIntersectionminus) / 2) % 360);
+
+		gyro.reset();
+
 		Button.waitForAnyPress();
 		System.exit(0);
 	}
--- src/ca/mcgill/ecse211/RingRetrieval/RingSearch.java
+++ src/ca/mcgill/ecse211/RingRetrieval/RingSearch.java
@@ -1,11 +1,11 @@
 package ca.mcgill.ecse211.RingRetrieval;
 
 public class RingSearch {
-	
+
 	public static void turnAroundTree()
 	{
-		
+
 	}
-	
+
 
 }
