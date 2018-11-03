3cd52d03e348799520b435eb5e5ea4558ad058af
chaimaef <chaimaefahmi@outlook.com>
2018-11-03 15:28:08 -0400

    Update Navigation Module :racehorse:

--- src/ca/mcgill/ecse211/Localization/LightLocalization.java
+++ src/ca/mcgill/ecse211/Localization/LightLocalization.java
@@ -23,35 +23,31 @@ public class LightLocalization {
 	private static double yOrigin;
 	private static double TRACK = FinalProject.getTrack();
 	private static double WHEEL_RAD = FinalProject.getWheelRad();
-<<<<<<< HEAD
-	private static final EV3LargeRegulatedMotor leftMotor = FinalProject.leftMotor;
-	private static final EV3LargeRegulatedMotor rightMotor = FinalProject.rightMotor;
-	private static final Odometer odo = FinalProject.odo;
-=======
+
 	private static final EV3LargeRegulatedMotor leftMotor = FinalProject.getLeftmotor();
 	private static final EV3LargeRegulatedMotor rightMotor = FinalProject.getRightmotor();
->>>>>>> db73b32... Cleaning the code
 	private static SampleProvider SColor = FinalProject.getColorBack();
 	private static float[] data = FinalProject.getColorBufferBack();
+	private static final double TILE_SIZE = FinalProject.getTileSize();
 	public static EV3GyroSensor gyro = FinalProject.gyro;
 	public static Odometer odo;
 
-	
-	/**
-	 * Rotate on place until all 4 line are detected, calculations done to determine what is the current position.
-	 * Navigate to the line intersection and  rotate to the 4th line heading and reset the gyro, 
-	 * this has to be done to calculate where is the 0 degree heading.
-	 * 
-	 * Reset 2 times, ~4 sec to reset.
-	 * Would be nice to find a way to reset only once and not twice.
-	 * 
-	 * Requirement: Needs to be close enough to an intersection so that when the robot rotates on place it detects all 4 lines.
-	 * 				Needs to be in the lower left quadrant to correct angle, but does not have to be in the lower left quadrant to correct X and Y.
-	 */
-	public static void lightLocalize() {
+	private static double treshold = 2 * TILE_SIZE;
 
-<<<<<<< HEAD
-=======
+	/**
+	 * Rotate in place until all 4 line are detected, calculations done to determine
+	 * what is the current position.Turn by the computed angle, then reset the gyro.
+	 * Travel to the origin.
+	 * 
+	 * Reset once, ~4 sec to reset.
+	 * 
+	 * Requirement: Needs to be close enough to an intersection so that when the
+	 * robot rotates on place it detects all 4 lines. Needs to be in the lower left
+	 * quadrant to correct angle, but does not have to be in the lower left quadrant
+	 * to correct X and Y.
+	 */
+
+	public static void lightLocalize(double x, double y, boolean positionOnly, double traveledDistance) {
 		try {
 			odo = Odometer.getOdometer(leftMotor, rightMotor, TRACK, WHEEL_RAD);
 		} catch (OdometerExceptions e) {
@@ -59,7 +55,9 @@ public class LightLocalization {
 			e.printStackTrace();
 		}
 
->>>>>>> db73b32... Cleaning the code
+		if (traveledDistance < treshold) {
+			return;
+		}
 		rightMotor.setSpeed(150);
 		leftMotor.setSpeed(150);
 
@@ -96,36 +94,33 @@ public class LightLocalization {
 			}
 		}
 
-		xOrigin = sensorOffset * Math.cos(Math.toRadians((yIntersectionminus - yIntersectionplus) / 2));
-		yOrigin = sensorOffset * Math.cos(Math.toRadians(xIntersectionplus - xIntersectionminus) / 2);
+		xOrigin = x + sensorOffset * Math.cos(Math.toRadians(yIntersectionminus - yIntersectionplus) / 2);
+		yOrigin = y + sensorOffset * Math.cos(Math.toRadians(xIntersectionplus - xIntersectionminus) / 2);
 
 		odo.setX(xOrigin);
 		odo.setY(yOrigin);
 
+		if (!positionOnly) {
+			Navigator.turnBy((((xIntersectionplus - xIntersectionminus) + 360) % 360) / 2);
+			gyro.reset();
+		}
+
 		Navigator.travelTo(0, 0);
 
 		Navigator.turnTo(xIntersectionminus);
-<<<<<<< HEAD
 
-		gyro.reset();
+		FinalProject.gyro.reset();
 
 		Navigator.turnTo((180 + (xIntersectionplus - xIntersectionminus) / 2) % 360);
 
-		gyro.reset();
-=======
-		
 		FinalProject.gyro.reset();
-	
-		Navigator.turnTo((180+(xIntersectionplus-xIntersectionminus)/2)%360);
-		
-		FinalProject.gyro.reset();
-		
+
 //		System.out.println("x final:" + odo.getXYT()[0]);
 //		System.out.println("y final:" + odo.getXYT()[1]);
 //		System.out.println("t final:" + odo.getXYT()[2]);
->>>>>>> db73b32... Cleaning the code
 
 		Button.waitForAnyPress();
 		System.exit(0);
 	}
+	
 }
