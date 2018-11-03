a064ffc902f78baefaca66c3594f03e7bfef4504
AnKhangC <ankhangchau98@gmail.com>
2018-11-03 12:55:30 -0400

    Clean Code :hammer:

--- src/ca/mcgill/ecse211/Localization/LightLocalization.java
+++ src/ca/mcgill/ecse211/Localization/LightLocalization.java
@@ -22,16 +22,42 @@ public class LightLocalization {
 	private static double yOrigin;
 	private static double TRACK = FinalProject.getTrack();
 	private static double WHEEL_RAD = FinalProject.getWheelRad();
+<<<<<<< HEAD
 	private static final EV3LargeRegulatedMotor leftMotor = FinalProject.leftMotor;
 	private static final EV3LargeRegulatedMotor rightMotor = FinalProject.rightMotor;
 	private static final Odometer odo = FinalProject.odo;
+=======
+	private static final EV3LargeRegulatedMotor leftMotor = FinalProject.getLeftmotor();
+	private static final EV3LargeRegulatedMotor rightMotor = FinalProject.getRightmotor();
+>>>>>>> db73b32... Cleaning the code
 	private static SampleProvider SColor = FinalProject.getColorBack();
 	private static float[] data = FinalProject.getColorBufferBack();
 	public static EV3GyroSensor gyro = FinalProject.gyro;
 
 	
+	/**
+	 * Rotate on place until all 4 line are detected, calculations done to determine what is the current position.
+	 * Navigate to the line intersection and  rotate to the 4th line heading and reset the gyro, 
+	 * this has to be done to calculate where is the 0 degree heading.
+	 * 
+	 * Reset 2 times, ~4 sec to reset.
+	 * Would be nice to find a way to reset only once and not twice.
+	 * 
+	 * Requirement: Needs to be close enough to an intersection so that when the robot rotates on place it detects all 4 lines.
+	 * 				Needs to be in the lower left quadrant to correct angle, but does not have to be in the lower left quadrant to correct X and Y.
+	 */
 	public static void lightLocalize() {
 
+<<<<<<< HEAD
+=======
+		try {
+			odo = Odometer.getOdometer(leftMotor, rightMotor, TRACK, WHEEL_RAD);
+		} catch (OdometerExceptions e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+>>>>>>> db73b32... Cleaning the code
 		rightMotor.setSpeed(150);
 		leftMotor.setSpeed(150);
 
@@ -77,12 +103,25 @@ public class LightLocalization {
 		Navigator.travelTo(0, 0);
 
 		Navigator.turnTo(xIntersectionminus);
+<<<<<<< HEAD
 
 		gyro.reset();
 
 		Navigator.turnTo((180 + (xIntersectionplus - xIntersectionminus) / 2) % 360);
 
 		gyro.reset();
+=======
+		
+		FinalProject.gyro.reset();
+	
+		Navigator.turnTo((180+(xIntersectionplus-xIntersectionminus)/2)%360);
+		
+		FinalProject.gyro.reset();
+		
+//		System.out.println("x final:" + odo.getXYT()[0]);
+//		System.out.println("y final:" + odo.getXYT()[1]);
+//		System.out.println("t final:" + odo.getXYT()[2]);
+>>>>>>> db73b32... Cleaning the code
 
 		Button.waitForAnyPress();
 		System.exit(0);
